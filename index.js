4b0115d6ada7ada18ccfa4205490947a38c7c325
NTElissa <ntihindukaelissa77@gmail.com>
2023-03-11 12:56:33 +0200

    new update

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs'
 import cors from 'cors'
 
-dotenv.config({ config: process.env.NODE_ENV === 'dev' });
-const db = process.env.NODE_ENV === "test" ? "mybrandtest" : "mybrandnodejs";
+// dotenv.config({ config: process.env.NODE_ENV === 'dev' });
+// const db = process.env.NODE_ENV === "test" ? "mybrandtest" : "mybrandnodejs";
 // const url = `mongodb://localhost/${db}`;
 // const xy = '';
-const url = `mongodb+srv://ntihinduka:ntihinduka@cluster0.i5jwzd6.mongodb.net/database?retryWrites=true`;
+// const url = `mongodb+srv://ntihinduka:ntihinduka@cluster0.i5jwzd6.mongodb.net/database?retryWrites=true`;
 
 const app = express();
 const port = process.env.PORT
