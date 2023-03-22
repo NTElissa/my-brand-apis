4525768cb775c7bdb4c25b1a75c6775e344e0a16
NTElissa <ntihindukaelissa77@gmail.com>
2023-03-22 09:10:38 +0200

    new updated

--- server.js
+++
@@ -1,9 +0,0 @@
-import app from "./index.js"
-import dotenv from "dotenv";
-
-dotenv.config({ config: process.env.NODE_ENV === 'dev' });
-const port = process.env.PORT
-
-app.listen(port, () =>{
-    console.log(`Server has started on port: ${port}`);
-})
